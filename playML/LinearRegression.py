bbb8caf6070f0338099d40bc1d693c91cb13bc6c
lvwei <lvwei@DESKTOP-T61L6QB>
2019-11-20 21:51:48 +0800

    sklearn中的梯度下降法完成线性回归

--- playML/LinearRegression.py
+++ playML/LinearRegression.py
@@ -63,6 +63,41 @@ class LinearRegression:
 
         return self
 
+    def fit_sgd(self, X_train, Y_train, n_iters=5, t0=5, t1=50):
+        assert X_train.shape[0] == Y_train.shape[0], "the size of X_train must be equal to the size of Y_train. "
+        assert n_iters >= 1
+
+        def dJ_sgd(theta, X_b_i, y_i):
+            return X_b_i.T.dot(X_b_i.dot(theta) - y_i) * 2.0
+
+        def sgd(X_b, y, init_theta, n_iters, t0=5, t1=50):
+            
+            def learning_rate(t):
+                return t0 / (t + t1)
+
+            theta = init_theta
+            m = len(X_b)
+
+            for cur_iter in range(n_iters):
+                indexes = np.random.permutation(m)
+                X_b_new = X_b[indexes]
+                y_new = y[indexes]
+
+                for i in range(m):
+                    gradient = dJ_sgd(theta, X_b_new[i], y_new[i])
+                    theta = theta - learning_rate(cur_iter * m + i) * gradient
+
+            return theta
+
+        X_b = np.hstack([np.ones((len(X_train), 1)), X_train])
+        initial_theta = np.zeros(X_b.shape[1])
+
+        self._theta = sgd(X_b, Y_train, initial_theta, n_iters, t0, t1)
+        self.interception_ = self._theta[0]
+        self.coef_ = self._theta[1:]
+
+        return self
+
     def predict(self, X_predict):
         assert self.interception_ is not None and self.coef_ is not None, "must fit before predict!"
         assert X_predict.shape[1] == len(self.coef_), "the feature number of X_predict must be equal to X_train"
